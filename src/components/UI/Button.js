377d3af0fe4b492f104cbac96e33ac97c0d04641
faisal-kn <khanmohdfaisal75@gmail.com>
2021-07-26 12:52:22 +0530

    Updated Header

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './Button.css';
 
 const Button = (props) => {
-    return (
-        <button className=""></button>
-    )
+  return (
+    <button className="button">
+      {props.value}
+      <span className="badge">{props.items}</span>
+    </button>
+  );
 };
 
 export default Button;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import './Header.css';
+import Button from '../UI/Button';
+import mealsImage from '../../assets/meals.jpg';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <div className="header">
-        React Meals
-        <button> Your Cart</button>
+        <h1>React Meals</h1>
+        <Button value="Your Cart" items="0"></Button>
       </div>
       <div className="main-image">
-        <img src="/" alt="A pizza image"></img>
+        <img src={mealsImage} alt="A pizza"></img>
       </div>
     </>
   );
