f7ef6cf5764961d2ff6c6651a39801c9e6473e73
faisal-kn <khanmohdfaisal75@gmail.com>
2021-07-26 13:48:55 +0530

    Added cart icon

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import './Button.css';
+import CartIcon from '../cart/CartIcon';
 
 const Button = (props) => {
   return (
     <button className="button">
+      <span className="icon">
+        <CartIcon />
+      </span>
       {props.value}
       <span className="badge">{props.items}</span>
     </button>
