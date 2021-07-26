be2162847d22f90023d7e2881e1001d043f2471a
faisal-kn <khanmohdfaisal75@gmail.com>
2021-07-26 23:36:15 +0530

    Added modal

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Cart = () => {
+  const cartItems = (
+    <ul>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className="total">
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className="actions">
+        <button className="button--alt">Close</button>
+        <button className="button">Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
